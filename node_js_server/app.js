4797df9b935fe9d69fce27270959b13de3fa22c4
he <411024092@qq.com>
2019-09-10 11:27:12 +0800

    app.js

--- node_js_server/app.js
+++ node_js_server/app.js
@@ -1,1 +1,6 @@
-console.log("33 w")
+const socket = require("socket.io");
+const app = socket('3000');
+app.on('cennection',function (socket) {
+    console.log('一个用户连接');
+});
+console.log('监听端口3000');
